8657e8d497b8636702b7e05728ff758f43c63d40
Matthew Hill <matthill265@gmail.com>
2023-03-13 15:51:35 -0600

    Creates the post model

---
+++ LaterGram/LaterGram/Model/Post.swift
@@ -0,0 +1,43 @@
+//
+//  Post.swift
+//  LaterGram
+//
+//  Created by Matthew Hill on 3/13/23.
+//
+
+import Foundation
+import UIKit
+
+class Post {
+    
+    let postTitle: String
+    let postBody: String
+    let postDate: Date
+    let postImage: UIImage?
+    let uuid: String
+    
+    var DictaryRepresentation: [String: AnyHashable] {
+        ["title": self.postTitle,
+         "post": self.postBody,
+         "date": self.postDate,
+         "image": self.postImage,
+         "uuid": self.uuid
+        ]
+    }
+    
+    init(postTitle: String, postBody: String, postDate: Date = Date(), postImage: UIImage?, uuid: String = UUID().uuidString) {
+        self.postTitle = postTitle
+        self.postBody = postBody
+        self.postDate = postDate
+        self.postImage = postImage
+        self.uuid = uuid
+    }
+}
+
+extension Post: Equatable {
+    static func == (lhs: Post, rhs: Post) -> Bool {
+        return lhs.uuid == rhs.uuid
+    }
+    
+    
+}
---
+++ LaterGram/LaterGram/Model/User.swift
@@ -0,0 +1,10 @@
+//
+//  User.swift
+//  LaterGram
+//
+//  Created by Matthew Hill on 3/13/23.
+//
+
+import Foundation
+
+
